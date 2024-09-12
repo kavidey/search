7ae472bbf46fa74861fe4ed48d1002833263dc98
kavidey <kaviasher@gmail.com>
2024-09-12 15:13:38 -0700

    Setup rusqulite

---
+++ src-tauri/src/database.rs
@@ -0,0 +1,58 @@
+use rusqlite::{Connection, Result};
+use std::fs;
+use tauri::AppHandle;
+
+const CURRENT_DB_VERSION: u32 = 1;
+
+#[derive(Debug)]
+struct File {
+    id: i32,
+    filename: String,
+    path: String,
+    filetype: String,
+    modified_date: i32,
+}
+
+pub fn initialize_database(app_handle: &AppHandle) -> Result<Connection, rusqlite::Error> {
+    let app_dir = app_handle
+        .path_resolver()
+        .app_data_dir()
+        .expect("The app data directory should exist.");
+    fs::create_dir_all(&app_dir).expect("The app data directory should be created.");
+    let sqlite_path = app_dir.join("search.sqlite");
+
+    let mut db = Connection::open(sqlite_path)?;
+
+    let mut user_pragma = db.prepare("PRAGMA user_version")?;
+    let existing_user_version: u32 = user_pragma.query_row([], |row| Ok(row.get(0)?))?;
+    drop(user_pragma);
+
+    upgrade_database_if_needed(&mut db, existing_user_version)?;
+
+    Ok(db)
+}
+
+/// Upgrades the database to the current version.
+pub fn upgrade_database_if_needed(
+    db: &mut Connection,
+    existing_version: u32,
+) -> Result<(), rusqlite::Error> {
+    if existing_version < CURRENT_DB_VERSION {
+        db.pragma_update(None, "journal_mode", "WAL")?;
+
+        let tx = db.transaction()?;
+
+        tx.pragma_update(None, "user_version", CURRENT_DB_VERSION)?;
+
+        tx.execute_batch(
+            "
+        CREATE TABLE items (
+          title TEXT NOT NULL
+        );",
+        )?;
+
+        tx.commit()?;
+    }
+
+    Ok(())
+}
